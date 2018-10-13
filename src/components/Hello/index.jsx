d2a8c250812a2655d07b8f945218aa5bed4de1fc
zhaozhiwen <zhaozhiwen@cai-inc.com>
2018-10-13 16:48:54 +0800

    update: 引入react依赖，webpack增加jsx支持

---
+++ src/components/Hello/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class Hello extends Component {
+  componentDidMount() {
+    // do sth
+  }
+  render() {
+    return <div>Hello, React</div>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import '@babel/polyfill'
-const elements = [1, 2, 3].map(item => {
-  return console.log('9999')
-})
+import React from 'react'
+import ReactDom from 'react-dom'
+import Hello from './components/Hello/index.jsx'
 
-console.log(elements)
-
-async function azumia() {
-  console.log('begin')
-  await new Promise(resolve => {
-    setTimeout(() => {
-      resolve()
-    }, 1000)
-  })
-  console.log('done')
-}
-azumia()
-
-console.log(Object.values({ 1: 2 }))
-
-console.log(Array.isArray([]))
+ReactDom.render(<Hello />, document.getElementById('app'))
